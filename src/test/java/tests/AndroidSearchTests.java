3ac4ecc4710480035b9078bfba9789363668e7c7
Radmir <yaradmir2203@gmail.com>
2022-08-13 00:26:15 +0500

    Add tests

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -11,6 +12,7 @@ import static io.qameta.allure.Allure.step;
 
 public class AndroidSearchTests extends TestBase {
 
+    @Tag("android")
     @Test
     public void searchArticle() {
         back();
